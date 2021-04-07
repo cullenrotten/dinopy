1cae7dfed5d10697e0caac64eaea769e2dbec450
Haikuno <mopprower@gmail.com>
2021-04-07 02:27:31 -0300

    time independent of fps test

--- Program.py
+++ Program.py
@@ -1,4 +1,4 @@
-import random, sys, pygame
+import random, sys, pygame, time
 from pygame.locals import *
 
 # Set up pygame
@@ -23,10 +23,13 @@ gray = (150,150,150)
 # Game variables
 lost = False
 score = 0.0
+lastTime = time.time()
+lastUpdate = 1.0
 FPS = 60
 
-# Set up time
-time = 200
+# Set up time bar
+timebarsize = 200
+
 # Set up movement variables
 MOVESPEED = 5
 INITJUMPFORCE = 200
@@ -79,185 +82,196 @@ gameStart()
 
 # Main game loop
 while True:
-    # EVENTS
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_LEFT or event.key == K_a:
-                moveLeft = True
-                moveRight = False
-            if event.key == K_RIGHT or event.key == K_d:
-                moveLeft = False
-                moveRight = True
-            if event.key == K_UP or event.key == K_w:
-                moveUp = True
-                moveDown = False
-            if event.key == K_DOWN or event.key == K_s:
-                moveUp = False
-                moveDown = True
-            if event.key == K_ESCAPE:
+    if lastUpdate >= 0.14:
+        score +=0.1
+        timebarsize -=0.25
+        lastUpdate = 0.0
+        if slow:
+            timebarsize -=0.25
+        if intang:
+            timebarsize -=0.5
+        # GAME VARIABLES
+        dt = time.time() - lastTime 
+        lastTime = time.time()
+        dt *= 60
+        timebar = pygame.Rect(20, 20, timebarsize, 20)
+        if timebarsize <= 0:
+            lost = True
+        # GAME VARIABLES END
+        
+        # EVENTS
+        for event in pygame.event.get():
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.key == K_TAB:
-                gameStart()
-                jumpForce = 0
-                moveLeft = False
-                moveRight = False
-                moveUp = False
-                moveDown = False
-                falling = False
-                stopJumping = False
-                intang = False
-                slow = False
-                jumpStep = 1 
-                score = 0
-                time = 200
-            if event.key == K_SPACE:
-                intang = True
-            if event.key == K_LSHIFT:
-                slow = True
-                FPS = 30
-                red = invertColor(red)
-                green = invertColor(green)
-                blue = invertColor(blue)
-                orange = invertColor(orange)
-                black = invertColor(black)
-                white = invertColor(white)
-                gray = invertColor(gray)
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT or event.key == K_a:
+                    moveLeft = True
+                    moveRight = False
+                if event.key == K_RIGHT or event.key == K_d:
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == K_w:
+                    moveUp = True
+                    moveDown = False
+                if event.key == K_DOWN or event.key == K_s:
+                    moveUp = False
+                    moveDown = True
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == K_TAB:
+                    gameStart()
+                    jumpForce = 0
+                    moveLeft = False
+                    moveRight = False
+                    moveUp = False
+                    moveDown = False
+                    falling = False
+                    stopJumping = False
+                    intang = False
+                    slow = False
+                    jumpStep = 1 
+                    score = 0
+                    timebarsize = 200
+                if event.key == K_SPACE:
+                    intang = True
+                if event.key == K_LSHIFT:
+                    slow = True
+                    red = invertColor(red)
+                    green = invertColor(green)
+                    blue = invertColor(blue)
+                    orange = invertColor(orange)
+                    black = invertColor(black)
+                    white = invertColor(white)
+                    gray = invertColor(gray)
+                if event.key == K_e:
+                    FPS = 30
 
-                
-        if event.type == KEYUP:
-            if event.key == K_LEFT or event.key == K_a:
-                moveLeft = False
-            if event.key == K_RIGHT or event.key == K_d:
-                moveRight = False
-            if event.key == K_UP or event.key == K_w:
-                moveUp = False
-                stopJumping = True
-            if event.key == K_DOWN or event.key == K_s:
-                moveDown = False
-            if event.key == K_SPACE:
-                intang = False
-            if event.key == K_LSHIFT:
-                slow = False
-                FPS = 60
-                red = invertColor(red)
-                green = invertColor(green)
-                blue = invertColor(blue)
-                orange = invertColor(orange)
-                black = invertColor(black)
-                white = invertColor(white)
-                gray = invertColor(gray)
-    # EVENTS END
+                    
+            if event.type == KEYUP:
+                if event.key == K_LEFT or event.key == K_a:
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == K_d:
+                    moveRight = False
+                if event.key == K_UP or event.key == K_w:
+                    moveUp = False
+                    stopJumping = True
+                if event.key == K_DOWN or event.key == K_s:
+                    moveDown = False
+                if event.key == K_SPACE:
+                    intang = False
+                if event.key == K_LSHIFT:
+                    slow = False
+                    red = invertColor(red)
+                    green = invertColor(green)
+                    blue = invertColor(blue)
+                    orange = invertColor(orange)
+                    black = invertColor(black)
+                    white = invertColor(white)
+                    gray = invertColor(gray)
+                if event.key == K_e:
+                    FPS = 60
+        # GAME VARIABLES END
 
-    # PLAYER MOVEMENT
-    if moveLeft and player.left != 0:
-        player.left -= MOVESPEED
-    elif moveRight and player.right != WINDOWWIDTH:
-        player.left += MOVESPEED
-    
-    # JUMP AND GRAVITY
-    if jumpForce <= 0:
-        falling = True
-    if player.bottom >= floor.top:
-        falling = False
-        jumpForce = 0
-        stopJumping = False
-        player.bottom = floor.top
-    if (
-            moveUp 
-        and not falling 
-        and player.top > WINDOWHEIGHT / 3 
-        and not stopJumping
-        ):
-        jumpForce = INITJUMPFORCE
-    if (
-            jumpStep != 60 or falling 
-        and player.bottom < floor.top
-        ):
-        jumpForce -= GRAVITY
-        player.top -= jumpForce / 30
-        jumpStep += 1
-    if jumpStep == 60 and not falling:
-        jumpStep = 1
-    # JUMP AND GRAVITY END
+        # PLAYER MOVEMENT
+        if moveLeft and player.left != 0:
+            player.left -= dt * MOVESPEED
+        elif moveRight and player.right != WINDOWWIDTH:
+            player.left += dt * MOVESPEED
+        # JUMP AND GRAVITY
+        if jumpForce <= 0:
+            falling = True
+        if player.bottom >= floor.top:
+            falling = False
+            jumpForce = 0
+            stopJumping = False
+            player.bottom = floor.top
+        if (
+                moveUp 
+            and not falling 
+            and player.top > WINDOWHEIGHT / 3 
+            and not stopJumping
+            ):
+            jumpForce = INITJUMPFORCE
+        if (
+                jumpStep != 30 or falling 
+            and player.bottom < floor.top
+            ):
+            jumpForce -= GRAVITY * dt
+            player.top -= (jumpForce / 30) * dt
+            jumpStep += 1
+        if jumpStep == 30 and not falling:
+            jumpStep = 1
+        # JUMP AND GRAVITY END
+        # PLAYER MOVEMENT END
 
-    # PLAYER MOVEMENT END
+        # WALL GENERATION
+        if len(walls) == 0:
+            walls.append(generateNewWall())
+        elif(
+                len(walls) < 3
+            and walls[len(walls)-1].right < WINDOWWIDTH - 150 
+            and random.randint(0,60) == 0
+            ):
+            walls.append(generateNewWall())
+        # WALL GENERATION END
 
-    # WALL GENERATION
-    if len(walls) == 0:
-        walls.append(generateNewWall())
-    elif(
-            len(walls) < 3
-        and walls[len(walls)-1].right < WINDOWWIDTH - 150 
-        and random.randint(0,60) == 0
-        ):
-        walls.append(generateNewWall())
-    # WALL GENERATION END
-
-    # WALL MOVEMENT
-    for wall in walls:
-        wall.left -= MOVESPEED / 2
-        if(wall.right <= 0):
-            walls.remove(wall)
-        elif Rect.colliderect(wall, player) and not intang:
-            lost = True
-    # WALL MOVEMENT END
-
-    # FRUIT GENERATION
-    if len(fruits) < 2:
-        fruits.append(generateNewFruit())
-    for wall in walls:
-        for i in range(len(fruits)):
-            if (
-                    wall.left < fruits[i].left < wall.right
-                or  len(fruits) == 2 and fruits[i-1].left < fruits[i].left < fruits[i-1].right
-                ):
-                fruits.remove(fruits[i])
-                fruits.append(generateNewFruit())
-    # FRUIT GENERATION END
-
-    # FRUIT MOVEMENT
-    for fruit in fruits:
-        fruit.left -= MOVESPEED / 2
-        if fruit.right <= 0:
-            fruits.remove(fruit)
-        elif fruit.colliderect(player) and not intang:
-            fruits.remove(fruit)
-            score += 15
-            time += 25
-    # FRUIT MOVEMENT END
-
-    # SCORE AND TIME
-    score +=0.1
-    time -=0.25
-    timebar = pygame.Rect(20, 20, time, 20)
-    if time <= 0:
-        lost = True
-    if intang:
-        time -=1
-    if slow:
-        time -=1
-    # UPDATE SCREEN
-    windowSurface.fill(black)
-    if len(walls) != 0:
+        # WALL MOVEMENT
+        if slow : dt /= 2
         for wall in walls:
-            pygame.draw.rect(windowSurface,red,wall)
-    if len(fruits) != 0:
+            wall.left -= dt * MOVESPEED / 2
+            if(wall.right <= 0):
+                walls.remove(wall)
+            elif Rect.colliderect(wall, player) and not intang:
+                lost = True
+        # WALL MOVEMENT END
+
+        # FRUIT GENERATION
+        if len(fruits) < 2:
+            fruits.append(generateNewFruit())
+        for wall in walls:
+            for i in range(len(fruits)):
+                if (
+                        wall.left < fruits[i].left < wall.right
+                    or  len(fruits) == 2 and fruits[i-1].left < fruits[i].left < fruits[i-1].right
+                    ):
+                    fruits.remove(fruits[i])
+                    fruits.append(generateNewFruit())
+        # FRUIT GENERATION END
+
+        # FRUIT MOVEMENT
         for fruit in fruits:
-            pygame.draw.rect(windowSurface,orange,fruit)
-    if time > 0:
-        pygame.draw.rect(windowSurface,blue,timebar)
-    pygame.draw.rect(windowSurface,green,floor)
-    if not intang:
-        pygame.draw.rect(windowSurface,white,player)
+            fruit.left -= dt * MOVESPEED / 2
+            if fruit.right <= 0:
+                fruits.remove(fruit)
+            elif fruit.colliderect(player) and not intang:
+                fruits.remove(fruit)
+                score += 15
+                timebarsize += 25
+        if slow : dt *= 2
+        # FRUIT MOVEMENT END
+
+        # UPDATE SCREEN
+        windowSurface.fill(black)
+        if len(walls) != 0:
+            for wall in walls:
+                pygame.draw.rect(windowSurface,red,wall)
+        if len(fruits) != 0:
+            for fruit in fruits:
+                pygame.draw.rect(windowSurface,orange,fruit)
+        if timebarsize > 0:
+            pygame.draw.rect(windowSurface,blue,timebar)
+        pygame.draw.rect(windowSurface,green,floor)
+        if not intang:
+            pygame.draw.rect(windowSurface,white,player)
+        else:
+            windowSurface.blit(intangSurface, (player.x, player.y))
+        pygame.display.update()
+        mainClock.tick(FPS)
+        # UPDATE SCREEN END
     else:
-        windowSurface.blit(intangSurface, (player.x, player.y))
-    pygame.display.update()
-    mainClock.tick(FPS)
-    # UPDATE SCREEN END
+        lastUpdate += dt
     
     # LOSE
     basicFont = pygame.font.SysFont(None, 24)
@@ -286,7 +300,6 @@ while True:
                         black = invertColor(black)
                         white = invertColor(white)
                         gray = invertColor(gray)
-                        FPS = 60
                     gameStart()
                     score = 0
                     lost = False
@@ -300,6 +313,6 @@ while True:
                     intang = False
                     slow = False
                     jumpStep = 1       
-                    time = 200
+                    timebarsize = 200
                     break
     # END LOSE
